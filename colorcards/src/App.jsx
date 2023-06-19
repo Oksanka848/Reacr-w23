65789dd7f353e1f356f7e90160798ba2579347ff
Oksanaka848 <okmitka@mail.ru>
2023-06-19 21:52:37 +0300

    created tarif cards

---
+++ colorcards/src/App.jsx
@@ -0,0 +1,18 @@
+
+import style from './App.css';
+import Tarif from './components/Tarif';
+import cards from './components/cards.json';
+
+export default function App (){
+  return ( 
+    <div className={style.App}>
+      {
+          cards.map((card) =>
+            <Tarif key={card.id} price={card.price} onClick={card.isSelected}
+            speed={card.speed}  
+            theme={card.theme} isFiltr={card.isFiltr}></Tarif>
+          )
+        }
+    </div>
+  );
+}
